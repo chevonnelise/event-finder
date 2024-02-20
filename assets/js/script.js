cd579186b7a40118aca46d7be70570be70610a71
chevonnelise <elise.chevonne@gmail.com>
2024-02-20 08:52:45 +0000

    edit search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const map = createMap('map', 1.3521, 103.8198);
+
+    document.querySelector("#searchButton").addEventListener("click", function(){
+        
+    })
 });
