68df1c542a2e06a0548fdfe34f8480e65ab4a935
chevonnelise <elise.chevonne@gmail.com>
2024-03-03 10:03:11 +0000

    add mouseover and mouseout event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,58 @@
 document.addEventListener("DOMContentLoaded", async function () {
-   
+
     // setup the map
     const map = L.map('map').setView([45.9432, 24.9668], 2);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
 
-       
-        const venueResponse = await axios.get('assets/data/venue.geojson');
-        const venueLayer = L.layerGroup();
-        venueLayer.addTo(map);
-        for (let venue of venueResponse.data.features) {
-            let reversedCoordinates = [venue.geometry.coordinates[1], venue.geometry.coordinates[0]]; // Reverse coordinates
-            
-            const venueIcon = L.icon({
+
+    const venueResponse = await axios.get('assets/data/venue.geojson');
+    const venueLayer = L.layerGroup();
+    venueLayer.addTo(map);
+    for (let venue of venueResponse.data.features) {
+        let reversedCoordinates = [venue.geometry.coordinates[1], venue.geometry.coordinates[0]]; // Reverse coordinates
+
+        const venueIcon = L.icon({
             iconUrl: 'assets/img/concert_location.png', // Path to your custom marker image
-            iconSize: [32, 32], // Size of the icon
+            iconSize: [40, 40], // Size of the icon
             iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
             popupAnchor: [0, -32] // Point from which the popup should open relative to the iconAnchor
-            });
-
-            // Create marker with venue icon
-            L.marker(reversedCoordinates, {icon: venueIcon}).bindPopup(`<h3>${venue.properties.Stadium}</h3><h5>${venue.properties.Country} (${venue.properties["City/State"]})</h5>`).addTo(venueLayer);
-        }
-
-        // var baseLayer1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //     attribution: '&copy; OpenStreetMap contributors'
-        // }).addTo(map);
-
-        var baseLayer2 = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; OpenTopoMap contributors'
         });
 
-        var overlayLayer = L.tileLayer('https://example.com/{z}/{x}/{y}.png', {
-            attribution: 'Your attribution here'
-        }).addTo(map);
+        // Create marker with venue icon
+        const marker = L.marker(reversedCoordinates, { icon: venueIcon }).bindPopup(`<h3>${venue.properties.Stadium}</h3><h5>${venue.properties.Country} (${venue.properties["City/State"]})</h5>`).addTo(venueLayer);
 
-        const baseLayers = {
-            "Base Layer 1": venueLayer,
-            "Base Layer 2": baseLayer2
-        };
+        // Add mouseover event listener
+        marker.on('mouseover', function (event) {
+            marker.openPopup(); // Open popup on mouseover
+        });
+        // Add mouseout event listener
+        marker.on('mouseout', function (event) {
+            marker.closePopup(); // Close popup on mouseout
+        });
 
-        const overlayLayers = {
-            "Overlay Layer": overlayLayer
-        };
+    }
 
-        L.control.layers(baseLayers, overlayLayers).addTo(map);
+    // var baseLayer1 = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; OpenStreetMap contributors'
+    // }).addTo(map);
+
+    var baseLayer2 = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenTopoMap contributors'
     });
-    
+
+    var overlayLayer = L.tileLayer('https://example.com/{z}/{x}/{y}.png', {
+        attribution: 'Your attribution here'
+    }).addTo(map);
+
+    const baseLayers = {
+        "Venue": venueLayer,
+        "Base Layer 2": baseLayer2
+    };
+
+    const overlayLayers = {
+        "Overlay Layer": overlayLayer
+    };
+
+    L.control.layers(baseLayers, overlayLayers).addTo(map);
+});
+
